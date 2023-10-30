676a39f56af9977d1b230ac19235417ba7c545c8
Dean Jackson <57651082+deanja@users.noreply.github.com>
2023-10-30 16:40:53 +1100

    Refactor and handle edge cases

--- musiccontrol/__main__.py
+++ musiccontrol/__main__.py
@@ -10,27 +10,31 @@ def take_input():
 
 
 def play_social_intrigue_music(spotify):
-    playlist_uri = (
-        "https://open.spotify.com/playlist/0L5LqTiW0NOjke9FhsTUyA?si=97c01770204e4808"
-    )
-
-    # Play them
-    player.play_playlist(spotify, playlist_uri, shuffle=True)
+    playlist_uri = "https://open.spotify.com/playlist/0L5LqTiW0NOjke9FhsTUyA"
+    result = player.play_selection(spotify, playlist_uri, shuffle=True)
+    if not result.is_success:
+        print(result.message)
 
 
 def play_celtic_bangers(spotify):
-    playlist_uri = "spotify:playlist:3HXdGvoKWYfjd6PxWvYb7D"
-    player.play_playlist(spotify, playlist_uri, shuffle=False)
+    playlist_uri = "https://open.spotify.com/playlist/3HXdGvoKWYfjd6PxWvYb7D"
+    result = player.play_selection(spotify, playlist_uri, shuffle=False)
+    if not result.is_success:
+        print(result.message)
 
 
 def play_neutral_music(spotify):
-    playlist_uri = "https://open.spotify.com/playlist/4vAFb3x82WjaE6Gqq5Doxm?si=0a3c612831c649b4"
-    player.play_playlist(spotify, playlist_uri, shuffle=True)
+    playlist_uri = "https://open.spotify.com/playlist/4vAFb3x82WjaE6Gqq5Doxm"
+    result = player.play_selection(spotify, playlist_uri, shuffle=True)
+    if not result.is_success:
+        print(result.message)
 
 
 def play_combat_music(spotify):
-    playlist_uri = "spotify:playlist:6FohP6m1ipvNjgllOH4HLt"
-    player.play_playlist(spotify, playlist_uri, shuffle=True)
+    playlist_uri = "https://open.spotify.com/playlist/6FohP6m1ipvNjgllOH4HLt"
+    result = player.play_selection(spotify, playlist_uri, shuffle=True)
+    if not result.is_success:
+        print(result.message)
 
 
 def fade_out_music():
--- musiccontrol/spotify/player.py
+++ musiccontrol/spotify/player.py
@@ -1,5 +1,8 @@
 import spotipy
 from spotipy import util
+from collections import namedtuple
+
+Result = namedtuple("Result", ["is_success", "message"])
 
 
 def get_spotify():
@@ -16,18 +19,33 @@ def get_spotify():
     return spotify
 
 
-def play_tracks(spotify, track_uris, shuffle=True):
+def play_selection(spotify, playlist_uri=None, track_uris=None, shuffle=True):
+    # Play selected music on Spotify.
+    # Requires either a playlist or list of tracks.
+
+    # For etiquette don't assume we can start playing on an inactive device.
+    if spotify.current_playback() == None:
+        return Result(
+            False, "No device active. Press Play on any Spotify device and try again."
+        )
+
+    if playlist_uri == track_uris == None:
+        return Result(False, "No playlist or track(s) selected.")
+
     spotify.shuffle(state=shuffle)
 
-    # DM wants the mood to change almost immediately, not at completion
-    # of currently playing track.
-    spotify.start_playback(uris=track_uris)
+    # Change playback now, not at completion of current track.
+    # todo: implement a fade of a few seconds if device supports volume adjustment.
+    spotify.start_playback(context_uri=playlist_uri, uris=track_uris)
+
+    return Result(True, "New selection now playing.")
 
 
-def play_playlist(spotify, playlist_uri, shuffle=True):
-    spotify.shuffle(state=shuffle)
-    spotify.start_playback(context_uri=playlist_uri)
+# def play_playlist(spotify, playlist_uri, shuffle=True):
+#     spotify.shuffle(state=shuffle)
+#     spotify.start_playback(context_uri=playlist_uri)
 
 
 def next_track(spotify):
+    # Start playing next track.
     spotify.next_track()
